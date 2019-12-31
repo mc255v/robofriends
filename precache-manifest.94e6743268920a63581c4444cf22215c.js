f2f09f7983949a2e70d6fea099e25e45351e79e0
Matthew Cook <mc255v@gmail.com>
2019-12-31 19:18:56 +0900

    Updates

---
+++ precache-manifest.94e6743268920a63581c4444cf22215c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "6f66386d3f5e91fa93d2",
+    "url": "/robofriends/static/css/main.9a19c1a0.chunk.css"
+  },
+  {
+    "revision": "6f66386d3f5e91fa93d2",
+    "url": "/robofriends/static/js/main.6f66386d.chunk.js"
+  },
+  {
+    "revision": "58f9198b2d910acf966c",
+    "url": "/robofriends/static/css/1.8926459c.chunk.css"
+  },
+  {
+    "revision": "58f9198b2d910acf966c",
+    "url": "/robofriends/static/js/1.58f9198b.chunk.js"
+  },
+  {
+    "revision": "1446d6ec194445c3572d",
+    "url": "/robofriends/static/js/runtime~main.1446d6ec.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "8a02fece6256bf3986a8747fd8fb8f76",
+    "url": "/robofriends/index.html"
+  }
+];
